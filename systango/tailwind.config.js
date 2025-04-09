52ae41f76a6596db26c5960b8fead0c6bf0d6f3c
kirti.varma <kirti.varma@techalchemy.com>
2025-04-09 11:56:07 +0530

    Remaining Files

---
+++ systango/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF5533',
+      },
+    },
+  },
+  plugins: [],
+  variants: {
+    extend: {},
+  },
+} 
